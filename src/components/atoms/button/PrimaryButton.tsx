be3b9a67a2414034de98caba70cdf2f7b60474e7
hara0510 <138122503+hara0510@users.noreply.github.com>
2024-05-24 15:54:06 +0900

    PrimaryButtonのコンポーネント分割

---
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -0,0 +1,19 @@
+import { memo, FC, ReactNode } from "react";
+import { Button, IconButton } from "@chakra-ui/react";
+
+type Props = {
+  children: ReactNode;
+}
+
+export const PrimaryButton: FC<Props> = memo ((props) => {
+  const { children } = props;
+  return (
+    <Button
+      bg="teal.400"
+      color="white"
+      _hover={{ opacity: 0.8 }}
+    >
+      {children}
+    </Button>
+  );
+});
