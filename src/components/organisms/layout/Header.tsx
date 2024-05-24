58672777ab948c76e095f8ed4d9f4229180c7524
hara0510 <138122503+hara0510@users.noreply.github.com>
2024-05-24 15:23:34 +0900

    メニュー遷移の実装

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,10 +1,23 @@
 import { Box, Flex, Heading, Link, useDisclosure } from "@chakra-ui/react";
-import { memo, FC } from "react";
+import { memo, FC, useCallback } from "react";
 import { MenuIconButton } from "../../atoms/button/MenuIconButton";
 import { MenuDrawar } from "../../moleculs/MenuDrawar";
+import { useNavigate } from "react-router-dom";
 
 export const Header: FC = memo (() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const navigate = useNavigate();
+
+  const onClickHome = useCallback(() => {
+    navigate("/home");
+  }, [navigate]);
+  const onClickUserManagement = useCallback(() => {
+    navigate("/home/user_management");
+  }, [navigate]);
+  const onClickSetting = useCallback(() => {
+    navigate("/home/setting");
+  }, [navigate]);
+
   return (
     <>
       <Flex
@@ -15,7 +28,7 @@ export const Header: FC = memo (() => {
       justify="space-between"
       padding={{ base: 3, md: 5 }}
       >
-        <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+        <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }} onClick={onClickHome}>
           <Heading
           as="h1"
           fontSize={{ base: "md",md: "lg" }}
@@ -30,9 +43,9 @@ export const Header: FC = memo (() => {
           display={{ base: "none" ,md: "flex"}}
         >
           <Box pr={4}>
-            <Link>ユーザー一覧</Link>
+            <Link onClick={onClickUserManagement}>ユーザー一覧</Link>
           </Box>
-            <Link>設定</Link>
+            <Link onClick={onClickSetting}>設定</Link>
         </Flex>
         <MenuIconButton onOpen={onOpen} />
       </Flex>
