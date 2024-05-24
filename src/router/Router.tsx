1d27785e7f96668656cc5d0bb80592d05e424b3c
hara0510 <138122503+hara0510@users.noreply.github.com>
2024-05-24 14:26:07 +0900

    ヘッダーの作成

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,13 +4,17 @@ import { Routes, Route } from "react-router-dom";
 import { Login } from "../components/pages/Login";
 import { homeRoutes } from "./HomeRoutes";
 import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 
 export const Router: FC = memo(()=>{
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       {homeRoutes.map((route) => (
-        <Route key={route.path} path={`home${route.path}`} element={route.element} />
+        <Route key={route.path} 
+         path={`home${route.path}`}
+         element={<HeaderLayout>{route.element}</HeaderLayout>}
+        />
       ))}
       <Route path="*" element={<Page404 />} />
     </Routes>
